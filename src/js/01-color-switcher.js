f712f48d3d0128746f88e0e773bd93086aa923e2
Kateryna <beeeeeeeeeelka@gmail.com>
2022-07-30 22:06:31 +0100

    1 task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const body = document.querySelector('body');
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
+stopButton.disabled = true;
+
+function onStartBtnClick () {
+    body.style.backgroundColor = getRandomHexColor();
+    startButton.disabled = true;
+    stopButton.disabled = false;
+
+    timerId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+};
+
+startButton.addEventListener('click', onStartBtnClick);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStopBtnClick () {
+    clearInterval(timerId);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+}
+
+stopButton.addEventListener('click', onStopBtnClick);
