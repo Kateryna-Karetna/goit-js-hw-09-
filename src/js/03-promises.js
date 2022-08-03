653be031679ae6b13cc30eab6c78c8601322b6a3
Kateryna <beeeeeeeeeelka@gmail.com>
2022-08-03 11:43:13 +0100

    bug

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,9 @@
+const form = document.querySelector('.form');
+form.addEventListener('input');
+
+const btn = document.querySelector('button');
+btn.addEventListener('submit', createPromise(position, delay));
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +12,6 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+// Напиши скрипт, який на момент сабміту форми викликає функцію createPromise(position, delay) стільки разів, скільки ввели в поле amount. Під час кожного виклику передай їй номер промісу (position), що створюється, і затримку, враховуючи першу затримку (delay), введену користувачем, і крок (step).
+
